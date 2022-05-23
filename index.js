98aaeb376ca9322b9e4eeb79b31b2abe25b39c32
armanovxx <iebeat2018@outlook.com>
2022-05-23 14:36:44 +0200

    hi2

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-const port = 3000;
-app.get("/run", (req, res) => {
-    res.send("Hi")
-})
-app.listen(port, () => {
-    console.log("Hi");
-})
